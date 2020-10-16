d30dbb305eb5c4d977f58a3db53014654c4c9197
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-16 18:50:25 -0500

    arrays have been added and user prompts updated

--- develop/assets/javascript/script.js
+++ develop/assets/javascript/script.js
@@ -1,38 +1,50 @@
-//User prompts that need to happen when the "generate password button is clicked"
-var promptCriteria = function () {
-	var charCount = prompt('How many characters would you like your pwd to be?');
-	var upperCase = confirm(
-		"Please click 'OK' if you would like to have UPPER 'CASE letters' in your generated password."
-	);
-	var lowerCase = confirm(
-		"Please click 'OK' if you would like to have 'lower case' letters in your generated password."
-	);
-	var specChar = confirm(
-		"Please click 'OK' if you would like to have special characters in your generated password."
-	);
-	var numbers = confirm(
-		"Please click 'OK' if you would like to have numbers in your generated password."
-	);
-	console.log(charCount, upperCase, lowerCase, specChar, numbers);
-};
 
-// Assignment Code
-var generateBtn = document.querySelector('#generate');
+
+//values that will be used to generate the random password
+var specials = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Write password to the #password input
 function writePassword() {
+	//----USER PROMPTS----//
+	charCount = parseInt(prompt('How many characters would you like your pwd to be? Please select a number between 8 and 128.'));
+	//check to see that a charCount number exists and determine if it is between 8 and 128
+		if (!charCount){
+			alert("Please enter a valid number between 8 and 128");
+			return writePassword();
+		}else if (charCount < 8 || charCount > 128) {
+			alert("Please enter a valid number between 8 and 128");
+			return writePassword();
+		}else {
+			upperCase = confirm("Please click 'OK' if you would like to have 'UPPER CASE' letters in your generated password.");
+			lowerCase = confirm("Please click 'OK' if you would like to have 'lower case' letters in your generated password.");
+			specChar = confirm("Please click 'OK' if you would like to have special characters in your generated password.");
+			numbers = confirm("Please click 'OK' if you would like to have numbers in your generated password.");
+		}
+	//check to see what values are being logged
+	console.log(charCount, upperCase, lowerCase, specChar, numbers);
+
 	var password = generatePassword();
 	var passwordText = document.querySelector('#password');
 
 	passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+
+//------------ASSIGN USER INPUT VARIABLES----------------//
+//values that will be defiend by the end user
+//created outside of a function so they can be called by other functions(global variables)
+var charCount;
+var upperCase;
+var lowerCase;
+var specChar;
+var numbers;
+
+//------------CALL THE FUNCTION----------------//
+//when the button is clicked, call the writePassword function
 generateBtn.addEventListener('click', writePassword);
 
-var criteria = {
-	upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-	lower: 'abcdefghijklmnopqrstuvwxyz',
-	special: '!@#$%^&*()',
-	number: '1234567890'
-};
+
