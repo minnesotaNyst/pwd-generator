9eed773afc7478f459deb0dd10fd9b95245a4c06
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-16 19:45:57 -0500

    comitting the code i have so i can switch branches

--- develop/assets/javascript/script.js
+++ develop/assets/javascript/script.js
@@ -8,11 +8,11 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Write password to the #password input
 function writePassword() {
 	//----USER PROMPTS----//
-	charCount = parseInt(prompt('How many characters would you like your pwd to be? Please select a number between 8 and 128.'));
+	charCount = parseInt(prompt('How many characters would you like your password to be? Please select a number between 8 and 128.'));
 	//check to see that a charCount number exists and determine if it is between 8 and 128
 		if (!charCount){
-			alert("Please enter a valid number between 8 and 128");
-			return writePassword();
+			alert("Please enter a valid number between 8 and 128 if you would like to continue...");
+			//return writePassword();
 		}else if (charCount < 8 || charCount > 128) {
 			alert("Please enter a valid number between 8 and 128");
 			return writePassword();
@@ -21,9 +21,17 @@ function writePassword() {
 			lowerCase = confirm("Please click 'OK' if you would like to have 'lower case' letters in your generated password.");
 			specChar = confirm("Please click 'OK' if you would like to have special characters in your generated password.");
 			numbers = confirm("Please click 'OK' if you would like to have numbers in your generated password.");
+
+			//check to see what values are being logged
+			console.log(charCount, upperCase, lowerCase, specChar, numbers);
+		};
+
+		//----CRITERIA CHECK----//
+		//the requirment is that at least one critera be selected in order to generate a password
+		if (!upperCase && !lowerCase && !specChar && !numbers){
+			passwordCriteria = alert("Please select at least one criteria...");
+			return writePassword(); //TODO: this could affect workflow negatively by sending the user back to the begining, work on adjusting this later
 		}
-	//check to see what values are being logged
-	console.log(charCount, upperCase, lowerCase, specChar, numbers);
 
 	var password = generatePassword();
 	var passwordText = document.querySelector('#password');
@@ -42,6 +50,8 @@ var upperCase;
 var lowerCase;
 var specChar;
 var numbers;
+
+var passwordCriteria;
 
 //------------CALL THE FUNCTION----------------//
 //when the button is clicked, call the writePassword function
