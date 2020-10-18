7529c45f57342f262e1e44dca7cf9b26b47250ed
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-18 15:15:28 -0500

    changes to file structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,7 @@
-
-// Assignment Code
 var generateBtn = document.querySelector('#generate');
 var copyBtn = document.querySelector("#copy")
 
-//------------ASSIGN USER INPUT VARIABLES----------------//
-//values that will be defiend by the end user
-//created outside of a function so they can be called by other functions(global variables)
+//----ASSIGN USER INPUT VARIABLES----//
 var charCount;
 var compiledChar;
 var newPassword = "";
@@ -22,33 +18,18 @@ var pwd = {
 	}
 }
 
-//values that will be used to generate the random password
+//----PASSWORD CHARACTER POSSIBILITIES----//
 var specials = ".'):~;!-#%*$@=>[(&{}<?]^+/";
 var charactersLower = "abcdefghijklmnopqrstuvwxyz";
 var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numChar = "1234567890";
 
-//variables that outline all possibilites for adding together characters based on user input
-var options ={
-	all: specials + charactersLower + charactersUpper + numChar,
-	slu: specials + charactersLower + charactersUpper,
-	sln: specials + charactersLower + numChar,
-	sun: specials + charactersUpper + numChar,
-	lun: charactersLower + charactersUpper + numChar,
-	sl: specials + charactersLower,
-	su: specials + charactersUpper,
-	sn: specials + numChar,
-	lu: charactersLower + charactersUpper,
-	ln: charactersLower + numChar,
-	un: charactersUpper + numChar,
-}
-
-//---WRITE THE PASSWORD CRITERIA----//
+//----GET THE PASSWORD CRITERIA----//
 function generatePassword() {
 	//create a reset so the end user does not get caught in an infinite loop
 	pwd.reset();
 	let newPassword = "";
-	//we need to gather the critera for the password in order to generate one
+	//gather the critera for the password in order to generate one
 	charCount = parseInt(prompt('How many characters would you like your password to be? Please select a number between 8 and 128.'));
 	//check to see that a charCount number exists and determine if it is between 8 and 128
 		if (!charCount){
@@ -62,9 +43,6 @@ function generatePassword() {
 			pwd.lowerCase = confirm("Please click 'OK' if you would like to have 'lower case' letters in your generated password.");
 			pwd.numChar = confirm("Please click 'OK' if you would like to have numbers in your generated password.");
 			pwd.specChar = confirm("Please click 'OK' if you would like to have special characters (#, %, *, $, @, etc.) in your generated password.");
-			
-			//check to see what values are being logged
-			//console.log(charCount, pwd.upperCase, pwd.lowerCase, pwd.specChar, pwd.numChar);
 		};
 
 		//----CRITERIA CHECK----//
@@ -73,53 +51,19 @@ function generatePassword() {
 			alert("Please select at least one criteria...");
 			generatePassword();
 		}
-		
-		if (pwd.specChar === true && pwd.lowerCase === true && pwd.upperCase === true && pwd.numChar === true){
-			compiledChar = options.all;
+		if(pwd.upperCase === true){
+			compiledChar += charactersUpper;
+		}
+		if(pwd.lowerCase === true){
+			compiledChar += charactersLower;
+		}
+		if(pwd.numChar === true){
+			compiledChar += numChar;
+		}
+		if(pwd.specChar === true){
+			compiledChar += specials;
+		}
 
-		} else if (pwd.specChar === true && pwd.lowerCase === true && pwd.upperCase === true){
-			compiledChar = options.slu;
-
-		} else if (pwd.specChar === true && pwd.lowerCase === true && pwd.numChar === true){
-			compiledChar = options.sln;
-
-		} else if (pwd.specChar === true && pwd.upperCase === true && pwd.numChar === true){
-			compiledChar = options.sun;
-
-		} else if (pwd.lowerCase === true && pwd.upperCase === true && pwd.numChar === true){
-			compiledChar = options.lun;
-
-		} else if (pwd.specChar === true && pwd.lowerCase === true){
-			compiledChar = options.sl;
-
-		} else if (pwd.specChar === true && pwd.upperCase === true){
-			compiledChar = options.su;
-
-		} else if (pwd.specChar === true && pwd.numChar === true){
-			compiledChar = options.sn;
-
-		} else if (pwd.lowerCase === true && pwd.upperCase === true){
-			compiledChar = options.lu;
-
-		} else if (pwd.lowerCase === true && pwd.numChar === true){
-			compiledChar = options.ln;
-
-		} else if (pwd.upperCase === true && pwd.numChar === true){
-			compiledChar = options.un;
-
-		} else if (pwd.upperCase === true){
-			compiledChar = charactersUpper;
-
-		} else if (pwd.lowerCase === true){
-			compiledChar = charactersLower;
-
-		} else if (pwd.numChar === true){
-			compiledChar = numChar;
-
-		} else if (pwd.specChar === true){
-			compiledChar = specials;
-		};
-		
 		//----GENERATE THE PASSWORD----//
 		for (i = 0; i < charCount; i++){
 		var char = Math.floor(Math.random() * compiledChar.length);
@@ -128,13 +72,13 @@ function generatePassword() {
 		return newPassword;
 }
 
-// Write password to the #password input
+// Write password to the text box
 function writePassword() {
 	let newPassword = generatePassword();
 	var passwordText = document.querySelector("#password");
   
 	passwordText.value = newPassword;
-  }  
+}  
 
 //copy to clipboard function
 function copyClip() {
@@ -143,7 +87,7 @@ function copyClip() {
 	document.execCommand("copy");
 }
 
-// Add event listener to generate button
+// Add event listener to generate and copy button
 generateBtn.addEventListener('click', writePassword);
 copyBtn.addEventListener('click', copyClip);
 
